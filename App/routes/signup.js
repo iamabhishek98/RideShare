d59d310eb93e1ecfc35af98df106754352f7b3b2
samuel787 <suther.david.samuel@gmail.com>
2019-10-23 00:44:10 +0800

    integrated password hashing with bcrypt

--- App/routes/signup.js
+++ App/routes/signup.js
@@ -2,16 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 const {Pool} = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'dtub2019',
-  port: 5432,
-})
 
-/* SQL Query */
-var sql_query = 'INSERT INTO users VALUES';
+const pool = new Pool({connectionString:process.env.DATABASE_URL})
+
+const bcrypt = require('bcrypt');
+
+/**
+ * Enter sql queries in here:
+ */
+
+const sql = {}
+sql.query = {
+    //registering
+    register: 'INSERT INTO users VALUES($1,$2,$3,$4)'
+}
+
 
 /* GET signup page. */
 router.get('/', function(req, res, next) {
@@ -19,23 +24,22 @@ router.get('/', function(req, res, next) {
 });
 
 // POST
-router.post('/', function(req, res, next){
+router.post('/', async function(req, res, next){
   //Retrieve Information
   var name = req.body.name;
   var password = req.body.password;
   var email = req.body.email;
   var credit_card = req.body.credit_card;
   var vehicle_num = req.body.vehicleNum;
-  console.log(name);
-  console.log(password);
-  console.log(email);
-  console.log(credit_card);
-  // Construct Specific SQL Query
-	var insert_query = sql_query + "('" + email + "','" + name + "','" + password + "','" +  credit_card+"')";
-  
-  
-	pool.query(insert_query, (err, data) => {
-		res.redirect('/login')
-	});
+
+  try{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    // Construct Specific SQL Query
+	  pool.query(sql.query.register, [email, name, hashedPassword, credit_card],(err, data) => {
+		  res.redirect('./login')
+	  });
+  } catch {
+    res.redirect('./signup')
+  }
 })
 module.exports = router;
