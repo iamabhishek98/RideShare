aeee96cd1472f676cfc59d75814a2196c125c4ca
samuel787 <suther.david.samuel@gmail.com>
2019-10-26 21:57:39 +0800

    added passenger and driver analytics pages

--- App/routes/signup.js
+++ App/routes/signup.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 const {Pool} = require('pg')
 
-//const pool = new Pool({connectionString:process.env.DATABASE_URL})
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'dtub2019',
-  port:5432,
-})
+const pool = new Pool({connectionString:process.env.DATABASE_URL})
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'dtub2019',
+//   port:5432,
+// })
 const bcrypt = require('bcrypt');
 
 /**
@@ -22,7 +22,6 @@ sql.query = {
     //registering
     register: 'INSERT INTO users VALUES($1,$2,$3,$4)'
 }
-
 
 /* GET signup page. */
 router.get('/', function(req, res, next) {
@@ -43,7 +42,7 @@ router.post('/', async function(req, res, next){
     // Construct Specific SQL Query
 	  pool.query(sql.query.register, [email, name, hashedPassword, credit_card],(err, data) => {
       console.log("query success");
-      res.redirect('./login')  
+      res.redirect('./login')
 	  });
   } catch {
     res.redirect('./signup')
