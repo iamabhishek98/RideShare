1f9aae60a9055d30aba1f3eba4bf30a5f4aa6e3d
priyan-coder <rajamohanpriyan@gmail.com>
2019-10-30 00:53:06 +0800

    WORKS

--- App/routes/message.js
+++ App/routes/message.js
@@ -53,8 +53,9 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 const sql = {};
 sql.query = {
-  //user_Message
-  message: "INSERT INTO message VALUES($1, $2, $3, $4, $5)"
+  //   //   //user_Message
+  message:
+    "INSERT INTO message (sender_email, receiver_email, msg, msg_time, msg_date) VALUES($1, $2, $3, $4, $5)"
 };
 
 /* GET signup page. */
@@ -63,27 +64,70 @@ router.get("/", function(req, res, next) {
 });
 
 // POST
-router.post("/", async function(req, res, next) {
+router.post("/basic", function(req, res, next) {
   //Retrieve Information
 
   var sender = "haha@haha.com";
   var email = req.body.email;
   var user_Message = req.body.user_Message;
   var d = new Date();
-  var time = d.getTime();
-  var date = d.getDate();
-
+  // var time = d.getTime();
+  // var date = d.getDate();
+  var time = "12:12:00";
+  var date = "14/12/2012";
   console.log(sender, email, user_Message, date, time);
-  try {
-    // Construct Specific SQL Query
-    pool.query(
-      sql.query.message,
-      [sender, email, user_Message, time, date],
-      (err, data) => {
-        console.log("sql query success");
-        console.log(data);
-      }
-    );
-  } catch {}
+  // var sql =
+  //   "insert into message values('" +
+  //   sender +
+  //   "', '" +
+  //   req.body.email +
+  //   "', + '" +
+  //   req.body.user_Message +
+  //   "'+'" +
+  //   time +
+  //   "', '" +
+  //   date +
+  //   "' )";
+
+  pool.query(
+    sql.query.message,
+    [sender, email, user_Message, time, date],
+    (err, data) => {
+      console.log(data);
+    }
+  );
+  // try {
+  //   const data = await pool.query(sql.query.message, [
+  //     sender,
+  //     email,
+  //     user_Message,
+  //     time,
+  //     date
+  //   ]);
+  //   console.log("hello");
+  //   console.log(data);
+  //   console.log(data.rows);
+  // } catch {}
+  // try {
+  //   // Construct Specific SQL Query
+  //   pool.query(
+  //     sql.query.message,
+  //     [sender, email, user_Message, time, date],
+  //     (err, data) => {
+  //       console.log("sql query success");
+  //       console.log(data);
+  //     }
+  //   );
+  // } catch {}
+
+  // pool.query("INSERT INTO message(sender_email, receiver_email,msg,msg_time,msg_date
+
+  // ")
 });
 module.exports = router;
+
+// sender_email varchar(256) references users(email) not null,
+// receiver_email varchar(256) references users(email) not null,
+// msg varchar(1024) not null,
+// msg_time time,
+// msg_date
