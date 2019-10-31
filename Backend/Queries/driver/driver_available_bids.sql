28f65a40052e2d99a53cbf64c0179af3c1f750e5
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-11-01 02:12:06 +0800

    driver current pax

--- Backend/Queries/driver/driver_available_bids.sql
+++ Backend/Queries/driver/driver_available_bids.sql
@@ -1,46 +1,34 @@
-/*select number of is_win bids*/
-/*find the number of total pax*/
-/*subtract the is_win from pax*/
-with W as (
-    select count(*) 
-    from bid 
-    where is_win is true
-    and email_driver = 'ayurenev5@icio.us'
-),
-
-P as (
-    select pax, B.vehicle
-    from vehicles V, bid B 
-    where V.license_plate = B.vehicle
-    and B.email_driver = 'ayurenev5@icio.us'
-)
-
-with N as (select distinct P.name, P.email
-from passenger P, bid B
-where P.email = B.email_bidder),
-
-CP as (select P.vehicle, P.pax-W.count as current_pax
-from (select count(*) 
-    from bid 
-    where is_win is true
-    and email_driver = 'ayurenev5@icio.us') W, 
-    (select pax, B.vehicle
-    from vehicles V, bid B 
-    where V.license_plate = B.vehicle
-    and B.email_driver = 'ayurenev5@icio.us') P)
-
 select distinct N.name, CP.current_pax, B.email_bidder, B.vehicle, B.start_loc, B.end_loc, B.amount, B.s_date, B.s_time
-from Bid B, (select distinct P.name, P.email
-from passenger P, bid B
-where P.email = B.email_bidder) N, 
-(select P.vehicle, P.pax-W.count as current_pax
-from (select count(*) 
-    from bid 
-    where is_win is true
-    and email_driver = 'ayurenev5@icio.us') W, 
-    (select pax, B.vehicle
-    from vehicles V, bid B 
-    where V.license_plate = B.vehicle
-    and B.email_driver = 'ayurenev5@icio.us') P)CP
+from Bid B, 
+    (select distinct P.name, P.email
+        from passenger P, bid B
+        where P.email = B.email_bidder) N,
+    (select distinct P.email_driver, P.vehicle, P.pax-W.count as current_pax
+        from 
+            (select Q1.email_driver, count(Q2.email_driver)
+                from 
+                    (select distinct email_driver, count(*)
+                    from bid
+                    group by email_driver) Q1
+                left join 
+                    (select distinct email_driver, count(*) 
+                    from bid 
+                    where is_win is true
+                    group by email_driver) Q2
+                on Q1.email_driver = Q2.email_driver
+                group by Q1.email_driver) W,  
+            (select distinct B.email_driver, B.vehicle, V.pax
+                from vehicles V, bid B 
+                where V.license_plate = B.vehicle) P
+where W.email_driver = P.email_driver) CP
 where B.email_bidder = N.email
+and B.email_driver = CP.email_driver
 and B.email_driver = 'ayurenev5@icio.us';
+
+
+-- select distinct N.name, B.email_bidder, B.vehicle, B.start_loc, B.end_loc, B.amount, B.s_date, B.s_time
+-- from Bid B, (select distinct P.name, P.email
+-- from passenger P, bid B
+-- where P.email = B.email_bidder) N
+-- where B.email_bidder = N.email
+-- and B.email_driver = 'ayurenev5@icio.us';
