d68dc125ded44d402fc38db6cfb2a490b3910907
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-11-08 17:35:45 +0800

    updating review and rating passengers done

--- App/routes/feedback.js
+++ App/routes/feedback.js
@@ -10,12 +10,14 @@ const pool = new Pool({connectionString:process.env.DATABASE_URL})
 const sql = []
 
 sql.query = {
-    list_trips: `select distinct P.name as driver, B.email_bidder, B.email_driver, B.e_date, B.e_time, B.start_loc, B.end_loc, B.review, B.rating 
+    list_trips: `select distinct P.name as driver, B.email_bidder, B.email_driver, B.s_date, B.s_time, B.e_date, B.e_time, B.start_loc, B.end_loc, B.review, B.rating 
                     from bid B, passenger P
                     where B.e_date is not null
                     and P.email = B.email_driver 
                     and B.email_bidder = $1
-                    order by B.e_date desc, B.e_time desc;`
+                    order by B.e_date desc, B.e_time desc;`,
+    insert_feedback: `update bid set review = $1, rating = $2 where s_date = $3 and s_time = $4 
+                        and email_bidder = $5 and email_driver = $6 and start_loc = $7;`,
 }
 var passenger_email;
 router.get('/', function(req, res, next){
@@ -42,14 +44,36 @@ router.get('/', function(req, res, next){
     
 })
 
-router.post('/submit_review', function(req, res, next){
-    var trip_id = req.body.trip_id;
+router.post('/submit_review', async function(req, res, next){
+    var id = req.body.trip_id;
+    var trip_id = id - 1;
     var review = req.body.review;
     var rating = req.body.rating;
-
     console.log(trip_id);
     console.log(review);
     console.log(rating);
+
+    var trip;
+    var list_trips = await pool.query(sql.query.list_trips, [passenger_email]);
+    if (list_trips != undefined) {
+        console.log(list_trips.rows)
+        trip = list_trips.rows[trip_id]
+    } else {
+        console.log('list of trips data is undefined')
+    }
+
+    if (trip != undefined) {
+        s_date = trip.s_date
+        s_time = trip.s_time
+        email_driver = trip.email_driver
+        start_loc = trip.start_loc
+        var insert_feedback = await pool.query(sql.query.insert_feedback, [review, rating, s_date, s_time, passenger_email, email_driver, start_loc])
+        if (insert_feedback != undefined) {
+            console.log(insert_feedback)
+        } else {
+            console.log('insert feedback data is undefined')
+        }
+    }
 
     res.redirect('./');
 })
