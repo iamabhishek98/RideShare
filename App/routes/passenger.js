c83134ca8025dac2bdcea211435e7a10c7f63d67
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-11-09 10:42:43 +0800

    ongoing trips trips page done

--- App/routes/passenger.js
+++ App/routes/passenger.js
@@ -80,7 +80,7 @@ sql.query = {
 
     favourite_location: `select * from favouriteLocation where email_passenger = $1`,
 
-    current_bids:`select P.name as driver, B.email_bidder, B.email_driver, B.start_loc, B.end_loc, B.s_date, B.s_time 
+    current_bids:`select P.name as driver, B.email_bidder, B.email_driver, B.start_loc, B.end_loc, B.s_date, B.s_time, B.amount, B.is_win 
                     from bid B, passenger P 
                     where B.email_driver = P.email 
                     and B.e_date is null 
